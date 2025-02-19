4a0afa53a164cd236309a78747b849b494bff1b9
sharu2126 <sharvarinalawade2@gmail.com>
2025-02-19 10:40:09 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,73 @@
+#1.Write a program to find the largest of two numbers using if-else statements.
+##a=20
+##b=25
+##
+##if(a>b):
+##    print(a)
+##else:
+##    print(b)
+
+#2.Write a program that uses if-else statements to print whether a number is positive, negative, or zero
+#a=int(input("enter a number"))
+#a=20
+##if(a>0):
+##    print("no is +ve")
+##elif(a<0):
+##    print("no is -ve")
+##else:
+##    print("no is zero")
+
+
+##3.Write a program that checks whether a given number is even or odd using the ternary operator.
+##a=7
+##if(a%2==0):
+##    print("given no is even")
+##else:
+##    print("given no is odd")
+#using ternary operator
+##no="even" if a%2==0 else "odd"
+##print(no)
+
+##4.result = 25 // 4 * 3 + 18 % 7 - 5 * 2 / 2
+##print(result)
+
+##5.Write a program to calculate the area of a triangle given its base and height using the formula Area = (base * height) / 2
+##height=20
+##base=15
+##a=height*base/2
+##print("Area of triangle:",a)
+
+##6.Write a program to calculate the perimeter of a rectangle using length and width variables.
+##a=int(input("enter a length"))
+##b=int(input("enter a width"))
+##p=2*(a+b)
+##print("Perimeter of reactangle:",p)
+
+##7.Write a program that uses the modulus operator (%) to find the remainder when dividing two numbers.
+##a=100
+##b=3
+##c=a%b
+##print(c)
+
+##8.Write a program to compare two numbers and print whether the first is greater, smaller, or equal to the second using relational operators.
+##a=100
+##b=125
+##if(a>b):
+##    print("1st number is greater than 2nd")
+##elif(a<b):
+##    print("1st number is not greater than 2nd")
+##elif(a==b):
+##    print("a and b are equal")
+##else:
+##    print(false)
+
+##9.Write a program that takes two integers and performs both floor division (//) and modulo (%) operations. Print the results
+##a=20
+##b=5
+##result1=(a//b)
+##result2=(a%b)
+##print("floor division:",result1)
+##print("modulo:",result2)
+
+
+
